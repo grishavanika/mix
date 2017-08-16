600d895282d9e8fe6d2505902ec3e3970a9e19f7
grisha vanika <grish.vanika@gmail.com>
2017-08-16 23:50:02 +0300

    Extend IParser interface: remember parse state - succesfull or not; what part of string was parsed

--- src/mixal_parser_lib/include/mixal/parser.h
+++ src/mixal_parser_lib/include/mixal/parser.h
@@ -8,17 +8,28 @@ class MIXAL_PARSER_LIB_EXPORT IParser
 {
 public:
 	std::size_t parse_stream(std::string_view str, std::size_t offset = 0);
+	
+	bool is_valid() const;
+	std::string_view str() const;
 
 protected:
+	IParser() = default;
 	~IParser() = default;
 
 	virtual void do_clear() = 0;
 	virtual std::size_t do_parse_stream(std::string_view str, std::size_t offset) = 0;
+
+private:
+	void clear();
+
+private:
+	std::string_view str_;
+	bool is_valid_{false};
 };
 
 inline std::size_t IParser::parse_stream(std::string_view str, std::size_t offset /*= 0*/)
 {
-	do_clear();
+	clear();
 
 	if (offset > str.size())
 	{
@@ -30,10 +41,29 @@ inline std::size_t IParser::parse_stream(std::string_view str, std::size_t offse
 	{
 		// Parser is in undetermined state.
 		// Put back in default state for free
-		do_clear();
+		clear();
+		return InvalidStreamPosition();
 	}
 
+	is_valid_ = true;
+	str_ = str.substr(offset, pos - offset);
 	return pos;
+}
+
+inline void IParser::clear()
+{
+	is_valid_ = false;
+	do_clear();
+}
+
+inline bool IParser::is_valid() const
+{
+	return is_valid_;
+}
+
+inline std::string_view IParser::str() const
+{
+	return str_;
 }
 
 } // namespace mixal
