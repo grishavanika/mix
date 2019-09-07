23bb3faa625dbf92652e29c7f195b0a5cdda1519
grisha vanika <grish.vanika@gmail.com>
2019-09-07 13:12:27 +0300

    Fix GCC on nix* build. Update Travis CI.
    * Handle SDL2 library installed in different ways
      (Vcpkg & apt-get install)
    * Build gl3w from sources manually in case it can't be found
      (i.e, was not installed by Vcpkg)

--- third_party/gl3w/gl3w_integration.cmake
+++ third_party/gl3w/gl3w_integration.cmake
@@ -1,7 +1,17 @@
 
-find_package(gl3w CONFIG REQUIRED)
 add_library(gl3w_Integrated INTERFACE)
-target_link_libraries(gl3w_Integrated INTERFACE unofficial::gl3w::gl3w)
+
+find_package(gl3w CONFIG QUIET)
+if (gl3w_FOUND)
+	message("Found gl3w with find_package()")
+	target_link_libraries(gl3w_Integrated INTERFACE unofficial::gl3w::gl3w)
+endif ()
+
+if (NOT gl3w_FOUND)
+	message("Downloading & building gl3w from sources")
+	add_subdirectory(gl3w)
+	target_link_libraries(gl3w_Integrated INTERFACE gl3w)
+endif ()
 
 if (clang_on_msvc)
 	target_compile_options(gl3w_Integrated INTERFACE
--- third_party/sdl2/sdl2_integration.cmake
+++ third_party/sdl2/sdl2_integration.cmake
@@ -1,8 +1,17 @@
 
 find_package(SDL2 CONFIG REQUIRED)
+
 add_library(SDL2_Integrated INTERFACE)
-target_link_libraries(SDL2_Integrated INTERFACE SDL2::SDL2)
 target_compile_definitions(SDL2_Integrated INTERFACE SDL_MAIN_HANDLED)
+
+# Vcpkg has SDL2::SDL2 
+if (TARGET SDL2::SDL2)
+	target_link_libraries(SDL2_Integrated INTERFACE SDL2::SDL2)
+else ()
+	# ... others (apt-get install libsdl2-dev) go old way
+	target_include_directories(SDL2_Integrated INTERFACE ${SDL2_INCLUDE_DIRS})
+	target_link_libraries(SDL2_Integrated INTERFACE ${SDL2_LIBRARIES})
+endif ()
 
 if (clang_on_msvc)
 	target_compile_options(SDL2_Integrated INTERFACE
