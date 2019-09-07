f6880f2bc98e2f40b526f8a6e3735cdf64515978
grisha vanika <grish.vanika@gmail.com>
2019-09-07 14:17:14 +0300

    Download and install SDL2 from sources in case find_package() fails.
    Should fix GCC & MinGW builds on *nix and Windows.

--- third_party/sdl2/sdl2_integration.cmake
+++ third_party/sdl2/sdl2_integration.cmake
@@ -1,22 +1,20 @@
 
-find_package(SDL2 CONFIG REQUIRED)
-
 add_library(SDL2_Integrated INTERFACE)
-target_compile_definitions(SDL2_Integrated INTERFACE SDL_MAIN_HANDLED)
 
-# Vcpkg has SDL2::SDL2 
+find_package(SDL2 CONFIG QUIET)
 if (TARGET SDL2::SDL2)
+	message("Found SDL2 with find_package()")
 	target_link_libraries(SDL2_Integrated INTERFACE SDL2::SDL2)
 else ()
-	# ... others (apt-get install libsdl2-dev) go old way
-	target_include_directories(SDL2_Integrated INTERFACE ${SDL2_INCLUDE_DIRS})
-	target_link_libraries(SDL2_Integrated INTERFACE ${SDL2_LIBRARIES})
+	message("Downloading & building SDL2 from sources")
+	add_subdirectory(sdl2)
+	target_link_libraries(SDL2_Integrated INTERFACE SDL2)
 endif ()
 
+target_compile_definitions(SDL2_Integrated INTERFACE SDL_MAIN_HANDLED)
 if (clang_on_msvc)
 	target_compile_options(SDL2_Integrated INTERFACE
 		-Wno-pragma-pack
 		-Wno-undef
 		-Wno-documentation)
 endif ()
-
